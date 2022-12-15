e22ff7bde3799fb846ce6c969c4bbdc5a6fc8b06
steph4211 <109760163+steph4211@users.noreply.github.com>
2022-12-14 23:27:40 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('☕️ Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
